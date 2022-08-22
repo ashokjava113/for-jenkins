396e89f9fd47e46cd22eda309449a80587f6be73
ashokjava113 <ashok.rathod@nablainfotech.com>
2022-08-22 18:29:22 +0530

    update files

--- src/main/java/com/jenkins/entity/User.java
+++ src/main/java/com/jenkins/entity/User.java
@@ -3,9 +3,11 @@ package com.jenkins.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @Data
@@ -13,7 +15,8 @@ import javax.persistence.Entity;
 @NoArgsConstructor
 public class User {
 
-    @Autowired
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String username;
     private String password;
